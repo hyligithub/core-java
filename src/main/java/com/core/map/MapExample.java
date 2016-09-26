c724a24d30a1b5e1a57aabe2fd3a0e24be2beb3a
lihuiyan3 <lihuiyan@jd.com>
2016-09-26 23:29:56 +0800

    map

--- src/main/java/com/core/map/MapExample.java
+++ src/main/java/com/core/map/MapExample.java
@@ -1,5 +1,6 @@
 package com.core.map;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -31,5 +32,19 @@ public class MapExample {
             System.out.println(entry.getKey() + "***" + entry.getValue());
         }
 
+        Map<String, String> map1 = new HashMap<>();
+        map1.putAll(map);
+        System.out.println(map1);
+
+        String nullKeyValue = map.remove(null);
+        System.out.println(nullKeyValue);
+        System.out.println(map1);
+
+        Set<String> keySet = map.keySet();
+        System.out.println(keySet);
+        Collection<String> values = map.values();
+        System.out.println(values);
+        map.clear();
+        System.out.println(map.isEmpty());
     }
 }
